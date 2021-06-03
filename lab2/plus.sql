066113989bf1e119967e04f74e6a0821d9ad5e46
Jasur Tursunov <tursunov.jasur@outlook.com>
2021-06-03 18:30:29 +0500

    th changed :|
    Signed-off-by: Jasur Tursunov <tursunov.jasur@outlook.com>

--- lab2/plus.sql
+++ lab2/plus.sql
@@ -1,54 +1,38 @@
-drop table if exists t1;
-drop table if exists t2;
-
-create table if not exists t1 
+create table Table1
 (
-	id int,
-	var1 char,
-	d_from date,
-	d_to date
+    id              int,
+    var1            varchar(30),
+    valid_from_dttm date,
+    valid_to_dttm   date
 );
 
-create table if not exists t2 
+create table Table2
 (
-	id int,
-	var2 char,
-	d_from date,
-	d_to date
+    id              int,
+    var2            varchar(30),
+    valid_from_dttm date,
+    valid_to_dttm   date
 );
 
-insert into t1 (id, var1, d_from, d_to) values (1, 'A', '20180901', '20180915');
-insert into t1 (id, var1, d_from, d_to) values (1, 'B', '20180916', '59991231');
+insert into Table1 values
+    (1, 'A', '2018-01-01', '2019-01-01'),
+    (1, 'B', '2019-01-01', '2019-01-01'),
+    (1, 'C', '2019-01-01', '5999-12-31');
 
-insert into t2 (id, var2, d_from, d_to) values (1, 'A', '20180901', '20180902');
-insert into t2 (id, var2, d_from, d_to) values (1, 'B', '20180903', '59991231');
 
-if t2 exists
-	select t1.id, t1.var1, t2.var2, 
-		case 
-		when t1.d_from<t2.d_from then t2.d_from
-			else t1.d_from
-		end as time1, 
-		case when t1.d_to>t2.d_to then t2.d_to
-			else t1.d_to 
-		end as time2
-	from t1 join t2 on 
-		t1.id = t2.id and 
-		t1.d_from<t2.d_to and 
-		t2.d_from<t1.d_to
-else
-	select t1.id, t1.var1, t1.d_from as time1, t1.d_to as time
-	from t1
+insert into Table2 values
+    (1, 'A', '2018-01-01', '2020-01-01'),
+    (1, 'B', '2020-01-02', '5999-12-31');
 
--- select t1.id, t1.var1, t2.var2, 
--- 	case 
--- 	when t1.d_from<t2.d_from then t2.d_from
--- 		else t1.d_from
--- 	end as time1, 
--- 	case when t1.d_to>t2.d_to then t2.d_to
--- 		else t1.d_to 
--- 	end as time2
--- from t1 join t2 on 
--- 	t1.id = t2.id and 
--- 	t1.d_from<t2.d_to and 
--- 	t2.d_from<t1.d_to
+
+select * from Table1;
+select * from Table2;
+
+
+select * from (
+        select T1.id as id, T1.var1 as var1, T2.var2 as var2,
+                greatest(T1.valid_from_dttm, T2.valid_from_dttm) as valid_from_dttm,
+                least(T1.valid_to_dttm, T2.valid_to_dttm) as valid_to_dttm
+        from Table1 T1 join Table2 T2 on T1.id = T2.id) as result
+where valid_to_dttm >= valid_from_dttm
+order by id;
